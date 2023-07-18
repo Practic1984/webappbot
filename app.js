1fe37dd00201eaa8a36c09fd3f1a23ee322d39c5
Practic <103997788+Practic1984@users.noreply.github.com>
2023-07-18 12:51:02 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-const formElement = document.getElementById('textinput'); // извлекаем элемент формы
+const formElement = document.getElementById('form1'); // извлекаем элемент формы
 formElement.addEventListener('submit', (e) => {
   e.preventDefault();
   const formData = new FormData(formElement); // создаём объект FormData, передаём в него элемент формы
   // теперь можно извлечь данные
-  const name = formData.get('fio'); // fio
-  const surname = formData.get('phone'); // phone
+  const fio = formData.get('fio'); // fio
+  const phone = formData.get('phone'); // phone
+  console.log(fio, phone)
 });
