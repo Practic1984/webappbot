dea0016daa20b0772c9c64aca01f60fded435702
Practic <103997788+Practic1984@users.noreply.github.com>
2023-07-18 13:48:45 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+
+let tg = window.Telegram.WebApp;
+tg.expand();
+
 const formElement = document.getElementById('form1'); // извлекаем элемент формы
 formElement.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -6,4 +10,16 @@ formElement.addEventListener('submit', (e) => {
   const fio = formData.get('fio'); // fio
   const phone = formData.get('phone'); // phone
   console.log(fio, phone)
+  Telegram.WebApp.onEvent('mainButtonClicked', function(){
+    tg.sendData(fio)
+    tg.sendData(phone)
+  }
 });
+
+// let tg = window.Telegram.WebApp;
+// tg.expand();
+// let item = "";
+// let fio = document.getElementById("fio1");
+// let phone = document.getElementById("phone1");
+// let email = document.getElementById("email");
+
