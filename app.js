11ca8f7440852937953d2be26bacbdc67643a85f
Practic <103997788+Practic1984@users.noreply.github.com>
2023-07-26 16:11:43 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -3,26 +3,26 @@ let tg = window.Telegram.WebApp; //получаем объект webapp теле
    tg.expand(); //расширяем на все окно  
 
    tg.MainButton.text = "Changed Text"; //изменяем текст кнопки 
-   tg.MainButton.setText("Отправить"); //изменяем текст кнопки иначе
+   tg.MainButton.setText("Changed Text1"); //изменяем текст кнопки иначе
    tg.MainButton.textColor = "#F55353"; //изменяем цвет текста кнопки
    tg.MainButton.color = "#143F6B"; //изменяем цвет бэкграунда кнопки
    tg.MainButton.setParams({"color": "#143F6B"}); //так изменяются все параметры
 
    let btn = document.getElementById("form1"); //получаем кнопку скрыть/показать 
 
-//    btn.addEventListener('click', function(){ //вешаем событие на нажатие html-кнопки
-//       if (tg.MainButton.isVisible){ //если кнопка показана 
-//         //  tg.MainButton.hide() //скрываем кнопку 
-//         let fio = document.getElementById("fio1");
-//         let phone = document.getElementById("phone1");
-//         let email = document.getElementById("email1");
-//         tg.sendData(JSON.stringify({fio: fio.value}))
-//         // tg.sendData(fio, phone, email)
-//       }
-//       else{ //иначе
-//          tg.MainButton.show() //показываем 
-//       }
-//    });
+   btn.addEventListener('click', function(){ //вешаем событие на нажатие html-кнопки
+      if (tg.MainButton.isVisible){ //если кнопка показана 
+        tg.MainButton.hide() //скрываем кнопку 
+        let fio = document.getElementById("fio1");
+        let phone = document.getElementById("phone1");
+        let email = document.getElementById("email1");
+        tg.sendData(JSON.stringify({fio: fio.value}, {phone: phone.value}, {email: email.value}))
+        // tg.sendData(fio, phone, email)
+      }
+      else{ //иначе
+         tg.MainButton.show() //показываем 
+      }
+   });
 
 
 
@@ -30,7 +30,7 @@ let tg = window.Telegram.WebApp; //получаем объект webapp теле
       let fio = document.getElementById("fio1");
       let phone = document.getElementById("phone1");
       let email = document.getElementById("email1");
-      tg.sendData(fio.value, phone.value, email.value)
+      tg.sendData(JSON.stringify({fio: fio.value}, {phone: phone.value}, {email: email.value}))
     //   tg.sendData(fio, phone, email); 
       //при клике на основную кнопку отправляем данные в строковом виде
    });
