c2b63a035dfadbd906cc0fc2644ad33dfcd37053
Practic <103997788+Practic1984@users.noreply.github.com>
2023-07-26 17:28:41 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ let tg = window.Telegram.WebApp; //получаем объект webapp теле
 
    tg.MainButton.text = "Отправить"; //изменяем текст кнопки 
 //    tg.MainButton.setText("Changed Text1"); //изменяем текст кнопки иначе
-   tg.MainButton.textColor = "#F55353"; //изменяем цвет текста кнопки
-   tg.MainButton.color = "#143F6B"; //изменяем цвет бэкграунда кнопки
+tg.MainButton.textColor = "#ffffff"; //изменяем цвет текста кнопки
+//    tg.MainButton.textColor = "#F55353"; //изменяем цвет текста кнопки
+   tg.MainButton.color = "#008000"; //изменяем цвет бэкграунда кнопки  #143F6B
 //    tg.MainButton.setParams({"color": "#143F6B"}); //так изменяются все параметры
 
    let btn = document.getElementById("form1"); //получаем кнопку скрыть/показать 
@@ -29,8 +30,8 @@ let tg = window.Telegram.WebApp; //получаем объект webapp теле
       let fio = document.getElementById("fio1");
       let phone = document.getElementById("phone1");
       let email = document.getElementById("email1");
-    //   tg.sendData(fio.value, phone.value, email.value)
-      tg.sendData({fio: fio.value, phone: phone.value, email: email.value})
+      tg.sendData(fio.value, phone.value, email.value)
+    // //   tg.sendData({fio: fio.value, phone: phone.value, email: email.value})
     //   tg.sendData(JSON.stringify({fio: fio.value, phone: phone.value, email: email.value}))
       tg.MainButton.hide()  
     //   tg.sendData(fio, phone, email); 
